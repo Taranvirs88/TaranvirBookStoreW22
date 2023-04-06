2424c68a8a0bd56b96e9944f270488b46311ec1d
Taranvirs88 <113557821+Taranvirs88@users.noreply.github.com>
2023-04-06 14:07:24 -0400

    Adding the CategoryControler.cs in the controller folder

--- TaranvirBookStore/Areas/Admin/Controllers/CategoryController.cs
+++ TaranvirBookStore/Areas/Admin/Controllers/CategoryController.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TaranvirBookStore.Controllers
+namespace TaranvirBookStore.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         public IActionResult Index()
