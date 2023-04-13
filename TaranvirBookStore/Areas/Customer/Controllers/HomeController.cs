54424d07919aff4e4367bc994d8f139a83bede12
Taranvirs88 <113557821+Taranvirs88@users.noreply.github.com>
2023-04-13 14:23:40 -0400

    Added the Category Controller and add a added the category.js files

--- TaranvirBookStore/Areas/Customer/Controllers/HomeController.cs
+++ TaranvirBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -28,6 +28,7 @@ namespace TaranvirBookStore.Area.Customer.Controllers
         {
             return View();
         }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
